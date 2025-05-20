a8f3bdbac1883293cd975a4fea63c9ac78c3c9ca
DmytroHalai <dimagalai04@gmail.com>
2025-05-20 14:08:10 +0300

    Add method, which adds row for brush shape
    This method saves not 4 coords, but creates a new table, which may be opened to check all the points of the brush

--- src/main/java/util/ShapeTable.java
+++ src/main/java/util/ShapeTable.java
@@ -12,10 +12,13 @@ public class ShapeTable extends JDialog {
     private final DefaultTableModel tableModel = new DefaultTableModel(new String[]{"Name", "x1", "y1", "x2", "y2", "Border Color", "Fill Color", "Thickness"}, 0);
     private final JTable myJTable = new JTable(tableModel);
     private final JFileChooser myJFileChooser = new JFileChooser(new File("."));
-    private File currentFile
+    private File currentFile;
 
     public void addRow(String name, int x1, int y1, int x2, int y2, String borderColor, String fillColor, int thickness) {
         tableModel.addRow(new Object[]{name, x1, y1, x2, y2, borderColor, fillColor, thickness});
     }
 
+    public void addBrushRow(String name, String x1, String y1, String x2, String y2, String borderColor, String fillColor, int thickness) {
+        tableModel.addRow(new Object[]{name, x1, y1, x2, y2, borderColor, fillColor, thickness});
+    }
 }
